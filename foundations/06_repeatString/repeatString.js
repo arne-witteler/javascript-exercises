0ecf40e3aef95193155f6f7a7dbeaf82cd8b5543
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-05-20 16:06:43 +0200

    add solution for 06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+  let sol = "";
+
+  if (num < 0) {
+    return 'ERROR';
+  } else {
+    for (let i = 0; i < num; i++) {
+      sol += string;
+    }
+  }
+  return sol;
 };
 
 // Do not edit below this line
